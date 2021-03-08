222e8ab53585aace7f55ce0673b75c244f6d781a
sonithanish <79008153+sonithanish@users.noreply.github.com>
2021-03-08 17:46:47 +0530

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,86 @@
+class Log {
+    constructor(x, y, height, angle) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, 20, height, options);
+      this.width = 20;
+      this.height = height;
+      Matter.Body.setAngle(this.body, angle);
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      strokeWeight(4);
+      stroke("green");
+      fill(255);
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
+
+  const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1, pig1;
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    
+    ground = new Ground(600,height,1200,20)
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    bird = new Bird(100,100);
+
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    pig3.display();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+
+    bird.display();
+}
+  
